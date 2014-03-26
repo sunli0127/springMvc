0f647d67ebe7b72fa619655daaf8f7a8f9f2fd18
sunli <sunli@ucweb.com>
2014-03-26 18:34:36 +0800

    去除不需要的target包

--- mvc-action/src/main/java/com/sl/mvc/HelloController.java
+++ mvc-action/src/main/java/com/sl/mvc/HelloController.java
@@ -2,6 +2,8 @@ package com.sl.mvc;
 
 import com.sl.dao.PayMntContactMapper;
 import com.sl.dao.payMntTypeMapper;
+import com.sl.domain.PayMntContact;
+import com.sl.domain.PayMntType;
 import com.sl.service.biz.RateService;
 import com.sl.service.remote.DubboTestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -30,11 +34,11 @@ public class HelloController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-//        List<PayMnType> payMntType = payMntTypeMapper.find(Arrays.asList("1","2"));
-
-//        PayMntContact payMntContact = payMntContactMapper.selectByPrimaryKey("majl");
+//        List<PayMntType> payMntType = payMntTypeMapper.find(Arrays.asList("1", "2"));
+        PayMntContact payMntType = payMntContactMapper.selectByPrimaryKey("majl");
         model.addAttribute("message", rateService.calRate());
-        model.addAttribute("message", dubboTestService.queryDog(""));
+//        model.addAttribute("message", dubboTestService.queryDog(""));
+        model.addAttribute("message", payMntType);
 		return "hello";
 	}
 
